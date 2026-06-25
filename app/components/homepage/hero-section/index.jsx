8583e742c2adae5a4324a11f4d1fe0ea6f009022
Abvhishek kumar <ojhaabhi13@gmail.com>
2026-06-26 00:42:18 +0530

    feat: add project, blog, and hero section components for homepage

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -26,8 +26,8 @@ function HeroSection() {
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hello, <br />
             This is {' '}
-            <span className="gradient-text">{personalData.name}</span>
-            {` , I'm a Professional `}
+            <span className="gradient-text uppercase">{personalData.name}</span>
+            {`, and I work as a `}
             <span className="text-[#16f2b3]">{personalData.designation}</span>
             .
           </h1>
